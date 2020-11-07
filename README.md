6b26db9c4785776b4c3fb247a12072de5051cd2a
grepM3 <mateusd.santos10@gmail.com>
2020-11-07 15:34:05 -0300

    add read.me

---
+++ README.md
@@ -0,0 +1,13 @@
+# Experiment Lab 🧪
+
+<p align="center"><img src="./src/assets/img/restaurant.png" width="320" height="320" /></p>
+
+## Objective
+<p style="font-size:14px;"><i> * Create a people counter for restaurant! </i></p>
+
+## Reference
+<p style="font-size:14px;"><i> * The basis of the project was created from the inspiration of the <a href="https://www.udemy.com/course/curso-completo-flutter-app-android-ios/learn/lecture/11093192?start=1#questions">Udemy: Flutter</a></i></p>
+
+## Features
+<p style="font-size:14px;"><i> * Flutter -> 
+Open source user interface development kit</i></p>
--- contador_pessoas/lib/main.dart
+++ contador_pessoas/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -29,7 +30,7 @@ class _State extends State<Home> {
 
         if (_people < 0) {
           _infoText = "Mundo Invertido?!";
-        } else if (_people <= 10) {
+        } else if (_people < 10) {
           _infoText = "Pode Entrar!";
         } else {
           _infoText = "O restaurante está com lotação!";
@@ -48,73 +49,89 @@ class _State extends State<Home> {
             fit: BoxFit.cover,
             height: MediaQuery.of(context).size.height,
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                "Pessoas: $_people",
-                style: GoogleFonts.balooBhaina(
-                  fontWeight: FontWeight.w400,
-                  color: Colors.white,
-                  decoration: TextDecoration.underline,
+          Center(
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.5,
+              width: MediaQuery.of(context).size.width * 0.8,
+              color: Colors.orangeAccent[700],
+              child: Card(
+                semanticContainer: true,
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(40.0),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    AutoSizeText(
+                      "Pessoas: $_people",
+                      style: GoogleFonts.balooBhaina(
+                        fontWeight: FontWeight.w400,
+                        fontSize: MediaQuery.of(context).size.width * 0.09,
+                        color: Colors.orangeAccent[700],
+                        decoration: TextDecoration.none,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: FlatButton(
+                            child: Text(
+                              "+1",
+                              style: GoogleFonts.balooBhaina(
+                                fontSize: 40.0,
+                                fontWeight: FontWeight.w200,
+                                color: Colors.orangeAccent[700],
+                                decoration: TextDecoration.none,
+                              ),
+                            ),
+                            onPressed: () {
+                              _changePeople(contPeople: 1);
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: FlatButton(
+                            child: Text(
+                              "-1",
+                              style: GoogleFonts.balooBhaina(
+                                fontSize: 40.0,
+                                fontWeight: FontWeight.w200,
+                                decoration: null,
+                                color: Colors.orangeAccent[700],
+                              ),
+                            ),
+                            onPressed: () {
+                              _changePeople(contPeople: -1);
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        AutoSizeText(
+                          _infoText,
+                          style: GoogleFonts.balooBhaina(
+                            fontSize: MediaQuery.of(context).size.width * 0.05,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.orangeAccent[700],
+                            decoration: TextDecoration.none,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: FlatButton(
-                      child: Text(
-                        "+1",
-                        style: GoogleFonts.balooBhaina(
-                          fontSize: 40.0,
-                          fontWeight: FontWeight.w200,
-                          color: Colors.white,
-                          decoration: TextDecoration.underline,
-                        ),
-                      ),
-                      onPressed: () {
-                        _changePeople(contPeople: 1);
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: FlatButton(
-                      child: Text(
-                        "-1",
-                        style: GoogleFonts.balooBhaina(
-                          fontSize: 40.0,
-                          fontWeight: FontWeight.w200,
-                          color: Colors.white,
-                        ),
-                      ),
-                      onPressed: () {
-                        _changePeople(contPeople: -1);
-                      },
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    _infoText,
-                    style: GoogleFonts.balooBhaina(
-                      fontSize: MediaQuery.of(context).size.width * 0.06,
-                      fontStyle: FontStyle.italic,
-                      color: Colors.white,
-                      decoration: TextDecoration.underline,
-                    ),
-                  ),
-                ],
-              ),
-            ],
+            ),
           ),
         ],
       ),
